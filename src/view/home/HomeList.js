a26af0e25b49e80d9055c8e7e4c0bf210e9e8520
siyunxin <15194113617@163.com>
2020-05-18 17:21:11 +0800

    分页调整

--- src/view/home/HomeList.js
+++ src/view/home/HomeList.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
 import { queryTopic } from '../../http/api'
-import { List, Avatar, Divider } from 'antd'
+import { List, Avatar, Divider, Pagination } from 'antd'
 export default class HomeList extends Component {
     constructor(arg) {
         super(arg)
         let path = this.props.location.pathname.split("/");
         this.state = {
             list: [],
+            totalPages: 0,
             page: 1,
-            limit: 20,
+            limit: 10,
             tab: path[path.length - 1]
         }
 
@@ -16,12 +17,15 @@ export default class HomeList extends Component {
     componentWillReceiveProps(nextProps) {
         let path = nextProps.location.pathname.split("/"),
             tab = path[path.length - 1]
-
-        this.updateList(tab, 1)
+        this.setState({
+            tab: tab,
+            page: 1
+        })
+        this.upDataList(tab, 1)
     };
     componentDidMount() {
         const { tab, page } = this.state;
-        this.updateList(tab, page)
+        this.upDataList(tab, page)
         /* const { tab, page, limit } = this.state;
         let data = {
             tab: tab,
@@ -33,7 +37,7 @@ export default class HomeList extends Component {
         }) */
     };
 
-    updateList(tab, page) {
+    upDataList(tab, page) {
         console.log(tab)
         const { limit } = this.state;
         let data = {
@@ -42,26 +46,45 @@ export default class HomeList extends Component {
             limit: limit,
         }
         queryTopic(data).then((res) => {
-            this.setState({ list: res.data })
+            this.setState(
+                {
+                    list: res.data,
+                    totalPages: res.data
+                }
+            )
         })
     }
     render() {
         return (
-            <List
-                dataSource={this.state.list}
-                renderItem={item => (
-                    <List.Item key={item.id}>
-                        <List.Item.Meta
-                            avatar={
-                                <Avatar src={item.author.avatar_url} />
-                            }
-                            title={<a href="https://ant.design">{item.title}</a>}
-                            description={<span>{item.author.loginname}发布于{item.create_at.split('T')[0]}</span>}
-                        />
-                        <div>访问{item.visit_count} <Divider type='vertical' /> 评论{item.reply_count}</div>
-                    </List.Item>
-                )}
-            />
+            <div>
+                <List
+                    dataSource={this.state.list}
+                    renderItem={item => (
+                        <List.Item key={item.id}>
+                            <List.Item.Meta
+                                avatar={
+                                    <Avatar src={item.author.avatar_url} />
+                                }
+                                title={<a href="https://ant.design">{item.title}</a>}
+                                description={<span>{item.author.loginname}发布于{item.create_at.split('T')[0]}</span>}
+                            />
+                            <div>访问{item.visit_count} <Divider type='vertical' /> 评论{item.reply_count}</div>
+                        </List.Item>
+                    )}
+                />
+                <Pagination
+                    defaultCurrent={this.state.page}
+                    total={500}
+                    onChange={(page) => {
+                        this.setState({
+                            page
+                        });
+                        this.upDataList(this.state.tab, page);
+                    }}
+                />
+
+            </div>
+
         )
     }
 }
